0716a73b8460bf2c3df234155c4d6d7d8094d589
dilip31 <dilipkhunti47@gmail.com>
2023-03-14 16:15:30 +0530

    upadating our category container component

---
+++ src/components/category-container/category-container.component.jsx
@@ -0,0 +1,27 @@
+
+import './category-container.syles.scss'
+
+
+const CategoryContainer =({category})=>{
+
+    const {imageUrl,title}=category;
+
+ return (
+
+    <div className="category-container">
+
+    <div className='background-image' style={{
+      backgroundImage: `url(${imageUrl})`
+    }}></div>
+
+    <div className="category-body-container">
+      <h2>{title}</h2>
+      <p>shop Now</p>
+    </div>
+
+  </div>
+)
+
+}
+
+export default CategoryContainer
